68fa56f0bae0f41bf1761bcf78aaca7992640bf1
Abolarin Olanrewaju Olabode <jabilegbe@gmail.com>
2018-06-29 17:58:53 +0100

    CRUD works

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 //friend component
 Vue.component('friend', {
-    props : ['friend'],
+    props : {
+        friend: Object,
+        index : Number,
+    },
     template: '#friend-template',
     data() {
         return {
@@ -8,12 +11,17 @@ Vue.component('friend', {
         }
     },
     methods : {
-        editFriend : function(){
-            this.isEditing = true;
-            
-        },
-        friendEdited : function(friend){
-            this.$emit('friend-edited', friend);
+       
+        friendEdited : function(){
+            //prepare the payload and send it in an event
+            const editedFriend = {};
+            editedFriend.name = this.$props.friend.name;
+            editedFriend.location = this.$props.friend.location;
+            editedFriend.gender = this.$props.friend.gender;
+            editedFriend.description = document.querySelector('#editedDescription').value.trim();
+            const id = this.$props.index;
+            // sending along the edited friend and its index
+            this.$emit('friend-edited', {editedFriend, id});
             this.isEditing = false;
         },
 
@@ -25,7 +33,9 @@ Vue.component('friend', {
 
 // stats component
 Vue.component('stats', {
-    props: ['noOfFriends'],
+    props: {
+        noOfFriends : Number,
+    },
     template: '#stats-template',
 });
 
@@ -33,34 +43,46 @@ Vue.component('stats', {
 const vm = new Vue({
     el : "#app",
     data : {
-        friends: [
-            { 
-                name:'Ojezele Ofure Peace',
-                gender: 'female',
-                location: 'Warri',
-                description:'My one true love'
-            },
-
-            { 
-                name:'Abolarin Oluwasegun Victor',
-                gender: 'male',
-                location: 'Abuja',
-                description:'My elder brother'
-            },
-            { 
-                name:'Tony Michael',
-                gender: 'male',
-                location: 'Abuja',
-                description:'My brother'
-            }
-        ]
+        friends: [],
+        addingFriend: false,
+        newFriend : {
+            name: '',
+            gender: '',
+            location: '',
+            description: ''
+        },
     },
     methods: {
-       
+       updateFriend: function(payload){
+            let friend = payload.editedFriend;
+            let index = payload.id;
+            this.friends[index] = friend;
+       },
+       addFriend : function(newFriend){
+        //    must make a shallow copy of the reactive object if i am going to clean it up after saving;
+        const friend =  {};
+        friend.name = newFriend.name;
+        friend.gender = newFriend.gender;
+        friend.location = newFriend.location;
+        friend.description = newFriend.description;
+
+        this.friends.push(friend);
+        this.newFriend.name = '';
+        this.newFriend.location ='';
+        this.newFriend.gender ='';
+        this.newFriend.description ='';
+
+        this.addingFriend = false;
+    
+        },
+
     },
     computed : {
         noOfFriends : function(){
             return this.friends.length;
+        },
+        orderedFriends: function(){
+            return this.friends.slice(0).reverse()
         }
     }
 })
